0957633af1f75380b9ec0435ef703f4742639237
blue007ms <46970805+blue007ms@users.noreply.github.com>
2019-01-29 01:30:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ var firstName = prompt("what is your first name?");
 var lastName = prompt("what is your last name?");
 var age = prompt("what is your age?");
 
+console.log("your Full Name is " + firstName + "" + lastName);
+console.log("your Are" + age + "" + "Years old");
